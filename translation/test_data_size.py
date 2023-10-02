70bac245ca573e18e87125a0fc671e31aa15baaa
MuhammadAlBarham <mohammedbrham98@gmail.com>
2023-10-02 11:42:42 +0400

    add testing file of the data size

--- translation/test_data_size.py
+++ translation/test_data_size.py
@@ -9,10 +9,43 @@ df = pd.read_feather("data/ccs_synthetic.feather")
 
 #%%
 print(len(df))
+#%%
+df = df[10:20]
+#%%
+df
+#%%
+# Check if you can retrieve index for 1M to 2M
+df.loc[0 + 10, "caption"]
 
 # %%
-print(len(df)/6)
+print(len(df) / 6)
 
 # %%
 
 # 2092750
+
+#%%
+# Test the generated translatio`n
+
+import pandas as pd
+
+df = pd.read_feather("data/ccs_synthetic_ar.feather")
+
+# %%
+df
+# %%
+import json
+
+
+def write_jsonl(df, output_file):
+    with open(output_file, "w") as file:
+        for index, row in df.iterrows():
+            # Convert each row to a dictionary and write it as a JSON line
+            json.dump(row.to_dict(), file)
+            file.write("\n")
+
+
+# %%
+write_jsonl(df, "ccs_synthetic_ar_1M.jsonl")
+
+# %%
